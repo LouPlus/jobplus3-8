f8d569978367d2c952168e5de3af89e248fe309a
uucloud <uucloud@qq.com>
2018-01-16 03:26:56 +0800

    fix #25 add database models

--- simplejob/models.py
+++ simplejob/models.py
@@ -1,14 +1,21 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+jobhunter_job = db.Table(
+    'jobhunter_job',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('job_id', db.Integer, db.ForeignKey('job.id', ondelete='CASCADE'))
+)
+
 class Base(db.Model):
     __abstract__ = True
 
-    created_at = db.Column(db.Datetime, default=datetime.utcnow)
-    updated_at = db.Column(db.Datetime, 
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, 
                         default=datetime.utcnow,
                         onupdate=datetime.utcnow)
 
@@ -21,12 +28,19 @@ class User(Base):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, index=True, nullable=False)
-    _password = db.Column('password', db.String(256), nullable=False)
+    _password = db.Column('password', db.String(128), nullable=False)
     email = db.Column(db.String(64), unique=True, index=True, nullable=False)
-    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    role = db.Column(db.SmallInteger, default=ROLE_JOBHUNTER)
+    job_intention = db.relationship('Job',
+                                secondary=jobhunter_job,
+                                backref='users')
+    # 该处是否可以使用外键
+    company_id = db.Column(db.Integer)
+    # 暂时先用简历地址代替简历存储
+    resume_url = db.Column(db.String(64))
 
     def __repr__(self):
-        return '<User:{}>'.format(self.username)
+        return '<User: {}>'.format(self.username)
 
     @property
     def password(self):
@@ -50,3 +64,38 @@ class User(Base):
     @property
     def is_jobhunter(self):
         return self.role == self.ROLE_JOBHUNTER
+
+
+class Company(Base):
+    __tablename__ = 'company'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    website = db.Column(db.String(64), nullable=False)
+    address = db.Column(db.String(64), nullable=False)
+    logo = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(128))
+    company_info = db.Column(db.Text)
+    
+    def __repr__(self):
+        return '<Company: {}'.format(self.name)
+
+
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, index=True)
+    salary = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(64), nullable=False)
+    # 职位要求    
+    requirement = db.Column(db.String(128))
+    # 职位描述
+    description = db.Column(db.Text)
+
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id', ondelete='CASCADE'))
+    company = db.relationship('Company', uselist=False, backref='jobs')
+
+    def __repr__(self):
+        return '<Job: {}'.format(self.name)
+
